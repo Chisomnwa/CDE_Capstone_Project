6e56db2c2cb0895401add6447fdc2ac633553e3e
Chisom Nnamani <nnamanichisomnpc@gmail.com>
2024-12-07 07:37:22 +0100

    Created redshift table schema

---
+++ airflow/dags/includes/redshift_table_schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.countries_table (
+    country_name VARCHAR(255),
+    independence BOOLEAN,
+    un_member BOOLEAN,
+    start_of_week VARCHAR(50),
+    official_name VARCHAR(255),
+    common_native_name VARCHAR(255),
+    currency_code VARCHAR(10),
+    currency_name VARCHAR(255),
+    currency_symbol VARCHAR(10),
+    country_code VARCHAR(10),
+    capital VARCHAR(255),
+    region VARCHAR(100),
+    sub_region VARCHAR(100),
+    languages TEXT,
+    area FLOAT,
+    population BIGINT,
+    continents TEXT
+);
